a50bcfb282043dc5c5d9814e9ebaac3b3ce6014f
megusadohara <84311092+megusadohara@users.noreply.github.com>
2022-07-20 03:52:42 +0900

    a

---
+++ docs/script.js
@@ -0,0 +1,18 @@
+window.onscroll = function(){  
+  var scrollTop = window.pageYOffset ;  //スクロール量を代入する
+  
+  if (scrollTop == 0 ) {   //最上部に戻ってきた時
+  $(".js-image").css('opacity', '1');
+    $('.js-image').css('transition', '0.5s')
+  
+    $(".git-image").css('opacity', '0');
+    $('.git-image').css('transition', '0.5s')
+  }
+  if (scrollTop > 10 ) {   //１０pxスクロールした時
+    $('.js-image').css('opacity', '0');
+    $('.js-image').css('transition', '0.5s')
+  
+    $('.git-image').css('opacity', '1');
+    $('.git-image').css('transition', '0.5s')
+  }
+}
