5c3c780d776cf0e4ad9ba83ed3f3ee7993523327
megusadohara <84311092+megusadohara@users.noreply.github.com>
2022-07-20 09:28:48 +0900

    やっと画像変わるようになった

--- docs/script.js
+++
@@ -1,9 +0,0 @@
-$(window).on('scroll', function () {
-  var doch = $(document).innerHeight(); //ページ全体の高さ
-  var winh = $(window).innerHeight(); //ウィンドウの高さ
-  var bottom = doch - winh; //ページ全体の高さ - ウィンドウの高さ = ページの最下部位置
-  if (bottom * 0.8 <= $(window).scrollTop()) {
-  //一番下までスクロールした時に実行
-  console.log("最底辺！");
-  }
-  });
