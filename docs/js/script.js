1c4dadb678146b76979d67be66cf1e9a6b345324
megusadohara <84311092+megusadohara@users.noreply.github.com>
2022-07-20 13:19:28 +0900

    a

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,20 +2,28 @@ window.onscroll = function(){
   var scrollTop = window.pageYOffset ;  //スクロール量を代入する
   
   if (scrollTop == 0 ) {   //最上部に戻ってきた時
-  $(".js-image").css('opacity', '1')
-    $('.js-image').css('transition', '10s')
+  $(".off-image").css('opacity', '1')
+    $('.off-image').css('transition', '10s')
+    
    
   
-    $(".git-image").css('opacity', '0')
-    $('.git-image').css('transition', '10s')
+    $(".on-image").css('opacity', '0')
+    $('.on-image').css('transition', '20s')
+    $(".box p2").css('opacity', '1')
+    $('.box p2').css('transition', '0.5s')
+    
   }
-  if (scrollTop > 2300 ) {   //１０pxスクロールした時
-    $(".js-image").css('opacity', '0')
-    $('.js-image').css('transition', '3s')
-
   
-    $(".git-image").css('opacity', '1')
-    $('.git-image').css('transition', '3s')
+  if (scrollTop > 2300 ) {   //１０pxスクロールした時
+    $(".off-image").css('opacity', '0')
+    $('.off-image').css('transition', '3s')
+    $(".box p").css('opacity', '0')
+    $('.box p').css('transition', '0.5s')
+  
+    $(".on-image").css('opacity', '1')
+    $('.on-image').css('transition', '3s')
+    
+   
   }
   }
 
