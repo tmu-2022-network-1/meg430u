60cec546f53446ce153fc92fca7e66300311b162
megusadohara <84311092+megusadohara@users.noreply.github.com>
2022-07-20 12:16:58 +0900

    a

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,16 +3,20 @@ window.onscroll = function(){
   
   if (scrollTop == 0 ) {   //最上部に戻ってきた時
   $(".js-image").css('opacity', '1')
-    $('.js-image').css('transition', '0.5s')
+    $('.js-image').css('transition', '10s')
+   
   
     $(".git-image").css('opacity', '0')
-    $('.git-image').css('transition', '0.5s')
+    $('.git-image').css('transition', '10s')
   }
-  if (scrollTop > 2000 ) {   //１０pxスクロールした時
+  if (scrollTop > 2300 ) {   //１０pxスクロールした時
     $(".js-image").css('opacity', '0')
-    $('.js-image').css('transition', '0.5s')
+    $('.js-image').css('transition', '3s')
+
   
     $(".git-image").css('opacity', '1')
-    $('.git-image').css('transition', '0.5s')
+    $('.git-image').css('transition', '3s')
   }
   }
+
+  
